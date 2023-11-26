581f69b45918753fbdcd666f893d96085d98867d
Slugslinger <davidemanuel_12@msn.com>
2023-11-26 19:02:10 +0000

    Movement
    Movement started
    Move on clicked only once

--- digman.ino
+++ digman.ino
@@ -1,11 +1,8 @@
-
 #include <Arduboy2.h>
 #include "bitmaps.h"
 
-// make an instance of arduboy used for many functions
 Arduboy2 arduboy;
 
-// make an ArdBitmap instance that will use the given the screen buffer and dimensions
 #define ARDBITMAP_SBUF arduboy.getBuffer()
 #include <ArdBitmap.h>
 ArdBitmap<WIDTH, HEIGHT> ardbitmap;
@@ -13,63 +10,80 @@ ArdBitmap<WIDTH, HEIGHT> ardbitmap;
 const int tileSize = 12;
 const int screenWidth = 128;
 const int screenHeight = 64;
-const int numBlocks = 10;  // Number of blocks you want to draw
+const int numBlocks = 10;
 
-// Define variables for element positions and other game logic
-// Adjust as needed based on your game design
+int blockYLevel1 = screenHeight - tileSize * 3;
+int blockYLevel2 = screenHeight - tileSize * 2;
+int blockYLevel3 = screenHeight - tileSize * 1;
+
+int totalBlockWidth = tileSize * numBlocks;
+int blockX = (screenWidth - totalBlockWidth) / 2;
+
+int digmanX = 50;
+int digmanY = blockYLevel1 - tileSize;
+
+bool isPressed = false;
+bool canMove = true;
 
 void setup() {
-  // initiate arduboy instance
   arduboy.begin();
   arduboy.setFrameRate(60);
 }
 
 void loop() {
+  arduboy.pollButtons(); // Read button inputs
+  
   if (!arduboy.nextFrame()) {
     return;
   }
 
-  // Game logic goes here
-
-  drawGame(); // Call function to draw game elements
-}
   
-// Define Y positions for each level of block2
-int blockYLevel1 = screenHeight - tileSize * 3;
-int blockYLevel2 = screenHeight - tileSize * 2;
-int blockYLevel3 = screenHeight - tileSize * 1;
+    if (arduboy.pressed(LEFT_BUTTON) || arduboy.pressed(RIGHT_BUTTON)) {
+      isPressed = true;
+    }
+    if (arduboy.notPressed(LEFT_BUTTON) && arduboy.notPressed(RIGHT_BUTTON)) {
+      isPressed = false;
+    }
+  
+    if(isPressed && canMove) {
+      if (arduboy.pressed(LEFT_BUTTON)) {
+        int newX = digmanX - tileSize;
 
-// Calculate the total width of the blocks
-int totalBlockWidth = tileSize * numBlocks;
-// Calculate the starting X position to center the blocks
-int blockX = (screenWidth - totalBlockWidth) / 2;
-
-// Digman Position
-int digmanX = 50;
-int digmanY = blockYLevel1 - tileSize; 
+        if (isAtXBoundary(newX)) {
+          digmanX = newX;
+        }
+      }
+      
+      else if (arduboy.pressed(RIGHT_BUTTON)) {
+        int newX = digmanX + tileSize;
+        if (isAtXBoundary(newX)) {
+          digmanX = newX;
+        }
+      }
+      canMove = false;
+    }
+    else if(!canMove && !isPressed) {
+      canMove = true;
+    }
+    // Similar logic for UP and DOWN movements...
+  
+  drawGame();
+}
 
 void drawGame() {
     arduboy.clear();
 
-    // Draw block2 for level 1
     for (int i = 0; i < numBlocks; i++) {
         arduboy.drawBitmap(blockX + i * tileSize, blockYLevel1, epd_bitmap_block2, 12, 12, WHITE);
-    }
-
-    // Draw block2 for level 2
-    for (int i = 0; i < numBlocks; i++) {
         arduboy.drawBitmap(blockX + i * tileSize, blockYLevel2, epd_bitmap_block2, 12, 12, WHITE);
-    }
-
-    // Draw block2 for level 3
-    for (int i = 0; i < numBlocks; i++) {
         arduboy.drawBitmap(blockX + i * tileSize, blockYLevel3, epd_bitmap_block2, 12, 12, WHITE);
     }
 
-    // Draw 'digman' character on top of the blocks
-    // int digmanX = 50; // Adjust this value for 'digman' X position
-    // int digmanY = blockYLevel1 - tileSize; // Position 'digman' above the blocks
     arduboy.drawBitmap(digmanX, digmanY, epd_bitmap_digman, 12, 12, WHITE);
 
     arduboy.display();
+}
+
+bool isAtXBoundary(int x) {
+    return (x >= 0 && x <= screenWidth - tileSize);
 }
