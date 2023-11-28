5520f6c11ae48fbfd87ed8e1d1a7c65cfb3c9425
Slugslinger <davidemanuel_12@msn.com>
2023-11-28 13:01:09 +0000

    Added time functionality
    Added time counter
    Added powerup bitmap
    Added powerup increasing time counter by 5 seconds

--- digman.ino
+++ digman.ino
@@ -24,6 +24,7 @@ int blockYLevels[numLevels];
 
 int currentLvl = 0;
 int score = 0;
+bool gameOver = false;
 
 int totalBlockWidth = tileSize * numBlocks;
 int blockX = (screenWidth - totalBlockWidth) / 2;
@@ -39,6 +40,11 @@ const unsigned long moveDelay = 150; // Delay between movements (in milliseconds
 unsigned long lastDownMoveTime = 0; // Timestamp to track last downward movement
 const unsigned long downMoveDelay = 150; // Delay between downward movements (in milliseconds)
 
+// Game timer
+unsigned long startTime;
+unsigned long currentTime;
+unsigned long timeElapsed;
+unsigned long gameDuration = 0.5 * 60000; // Game duration in milliseconds
 void setup() {
   // Initialize block Y positions
   for (int i = 0; i < numLevels; i++) {
@@ -52,6 +58,8 @@ void setup() {
 
   arduboy.begin();
   arduboy.setFrameRate(60);
+
+   startTime = millis(); // Record the start time when the game begins
 }
 
 void loop() {
@@ -61,17 +69,32 @@ void loop() {
     return;
   }
 
-  if (arduboy.pressed(LEFT_BUTTON)) {
-    moveDigman(-tileSize);
-  } else if (arduboy.pressed(RIGHT_BUTTON)) {
-    moveDigman(tileSize);
-  }
-  
-  if (arduboy.pressed(DOWN_BUTTON)) {
-    moveBlocksUp();
-  }
+  if (!gameOver) {
+    if (arduboy.pressed(LEFT_BUTTON)) {
+      moveDigman(-tileSize);
+    } else if (arduboy.pressed(RIGHT_BUTTON)) {
+      moveDigman(tileSize);
+    }
+    
+    if (arduboy.pressed(DOWN_BUTTON)) {
+      moveBlocksUp();
+    }
 
-  drawGame();
+    if (timeElapsed >= gameDuration) {
+      gameOver = true;
+      // Optionally, you can add further actions here when the game ends
+    }
+
+    drawGame();
+  } else {
+    // Game over, display final score fullscreen
+    displayFinalScore();
+    // Optionally, you can add a reset or exit condition here
+
+    if (arduboy.pressed(A_BUTTON)) {
+      resetGame();
+    }
+  }
 }
 
 void displayScore() {
@@ -93,7 +116,8 @@ void displayScore() {
 
   int scoreX = screenWidth - 2 - (scoreDigits * 6); // Adjust the multiplication factor based on font size
 
-  arduboy.fillRect(scoreX - 6, 0, scoreDigits * 6, 8, BLACK); // Black background for the score
+  //Adjust the -6 in the fillRect function to increase or decrease the padding between the score and the score label.
+  arduboy.fillRect(scoreX - 2, 0, scoreDigits * 6, 8, BLACK); // Black background for the score
   arduboy.setCursor(scoreX, 2); // Adjust position for the score text
   arduboy.print(score);
 }
@@ -115,9 +139,11 @@ void moveDigman(int deltaX) {
 int getRandomValue() {
   int randomValue = random(1, 101); // Generate a random number between 1 and 100
 
-  if (randomValue <= 75) {
+  if (randomValue <= 2) {
+    return 5;
+  } else if (randomValue <= 75) {
     return 0; // Around 75% chance for a gem
-  } else if (randomValue <= 92) {
+  } else if (randomValue <= 90) {
     return 1; // Around 5% chance for a gem
   } else if (randomValue <= 99) {
     return 10; // Around 15% chance for a 10-point gem
@@ -158,7 +184,9 @@ void drawGame() {
         
         if(newX == digmanX && newY == digmanY){
           // Add score
-          if(blocks[i][j] > 0) {
+          if(blocks[i][j] == 5) {
+            gameDuration += 5000; //more 5 seconds
+          } else if(blocks[i][j] > 0) {
             score += blocks[i][j];
           }
 
@@ -168,6 +196,9 @@ void drawGame() {
 
         if (blocks[i][j] == -1) {
           arduboy.drawRect(newX, newY, tileSize, tileSize, BLACK); // Draw black rectangle for an empty block
+        }
+        else if(blocks[i][j] == 5) {
+          arduboy.drawBitmap(newX, newY, epd_bitmap_time, 12, 12, WHITE);
         }
         else if(blocks[i][j] == 1) {
           arduboy.drawBitmap(newX, newY, epd_bitmap_gem2, 12, 12, WHITE);
@@ -187,9 +218,54 @@ void drawGame() {
 
     arduboy.drawBitmap(digmanX, digmanY, epd_bitmap_digman, 12, 12, WHITE);
 
+    currentTime = millis();
+    timeElapsed = currentTime - startTime;
+
     displayScore();
+    displayTimer();
 
     arduboy.display();
+}
+
+void displayTimer() {
+  int timeDigits = 2; // Display timer in seconds (2 digits)
+  int secondsLeft = (gameDuration - timeElapsed) / 1000; // Convert milliseconds to seconds
+  
+  int timerX = 2; // Adjust this value for positioning the timer
+
+  arduboy.fillRect(timerX, 0, timeDigits * 6, 8, BLACK); // Black background for the timer
+  arduboy.setCursor(timerX, 2); // Adjust position for the timer text
+  arduboy.print(secondsLeft);
+}
+
+void displayFinalScore() {
+  arduboy.clear();
+  arduboy.setCursor(10, 10); // Adjust position for the final score text
+  arduboy.print("Game Over!");
+  arduboy.setCursor(10, 20); // Adjust position for the final score text
+  arduboy.print("Final Score:");
+  arduboy.setCursor(50, 30); // Adjust position for the final score value
+  arduboy.print(score);
+  arduboy.setCursor(10, 50); // Adjust position for the instructions text
+  arduboy.print("Press A to restart");
+  arduboy.display();
+}
+
+void resetGame() {
+  // Reset all game variables to their initial values
+  gameOver = false;
+  score = 0;
+  startTime = millis();
+  gameDuration = 0.5 * 60000;
+  // Add any additional reset logic here if needed
+
+  // Re-initialize block Y positions and values
+  for (int i = 0; i < numLevels; i++) {
+    blockYLevels[i] = tileSize * (startingLvl + i) + blockY;
+    for (int j = 0; j < numBlocks; j++) {
+      blocks[i][j] = getRandomValue();
+    }
+  }
 }
 
 bool isAtXBoundary(int x) {
